8fea616d8f128f6d9a9701dc5d1da13701f48114
stephentriphahn <stephentriphahn@gmail.com>
2015-01-20 09:53:13 -0500

    added dropdown to side menu

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,10 @@
  * Controller of the reportlistApp
  */
 angular.module('reportlistApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function () {
+		this.open = false;
+		
+		this.showOptions = function(){
+			this.open = !this.open;
+		}
   });
